c294f3d189befa5d89dd2a33d0cf85d4c9081944
Bryan Ricker <bricker88@gmail.com>
2012-03-14 00:33:08 -0700

    Updating seeds and config file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create name: "LC Admin", email: Config["company"]["email"], password: "lcadmin", profile_type: "Admin" # FIXME password is overwritten
+user = User.create name: "LC Admin", email: Config["company"]["email"], profile_type: "Admin"
+user.password = "lcadmin"
+user.password_confirmation = "lcadmin"
+user.save
